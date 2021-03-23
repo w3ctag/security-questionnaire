6bf54c003edadd7b544be8170d6d5fe925908cca
Piotr Bialecki <bialpio@o2.pl>
2021-03-23 08:59:19 -0700

    Minor fixes (#124)
    Fix typo in generate-markdown.py, remove double space (question 9), add
    question mark (question 8), regenerate questionnaire.markdown.

--- generate-markdown.py
+++ generate-markdown.py
@@ -30,7 +30,7 @@ class MarkdownGenerator:
 
 This questionnaire has [moved](https://w3ctag.github.io/security-questionnaire/).
 
-For your convenience, a copy of the questionnaire's questions is quoted here in Markdwon, so you can easily include your answers in an [explainer](https://github.com/w3ctag/w3ctag.github.io/blob/master/explainers.md).
+For your convenience, a copy of the questionnaire's questions is quoted here in Markdown, so you can easily include your answers in an [explainer](https://github.com/w3ctag/w3ctag.github.io/blob/master/explainers.md).
 """, file=outfile)
         [self.process_line(line, outfile) for line in infile]
 
